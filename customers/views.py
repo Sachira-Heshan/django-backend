b22a2696881ed68c3961660f5b86a79cb8d572fa
Sachira-Heshan <sachiraheshan99@gmail.com>
2022-12-21 23:25:09 +0530

    jwt

--- customers/views.py
+++ customers/views.py
@@ -1,12 +1,13 @@
 from customers.models import Customer
-from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def customers(request):
     # invoke serializer and return to client
     if request.method == 'GET':
@@ -23,6 +24,7 @@ def customers(request):
 
 
 @api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def customer(request, id):
     # invoke serializer and return to client
     try:

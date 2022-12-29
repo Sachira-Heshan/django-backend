025c2201f4c9016827661e0953983063bfbec763
Sachira-Heshan <sachiraheshan99@gmail.com>
2022-12-29 09:58:19 +0530

    add orders

--- customers/models.py
+++ customers/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     industry = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, related_name="orders", on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    totalInCents = models.IntegerField()
+
+    def __str__(self):
+        return self.description
